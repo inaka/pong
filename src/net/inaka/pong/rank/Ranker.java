6aa026aa4c0bffc28afe193a6a4e74e0064ac6a8
Brujo Benavides <elbrujohalcon@gmail.com>
2014-02-13 14:17:08 -0300

    Tournament count reset

--- src/net/inaka/pong/rank/Ranker.java
+++ src/net/inaka/pong/rank/Ranker.java
@@ -377,7 +377,7 @@ public class Ranker {
 
 			List<WorksheetEntry> worksheets = inakaPongSheet.getWorksheets();
 			Double age = 0.0;
-			int tournaments = 4;
+			int tournaments = 1;
 			for (WorksheetEntry worksheet : worksheets) {
 				if (tournaments == 0)
 					break;
