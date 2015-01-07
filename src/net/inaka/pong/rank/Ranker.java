afac85c3689a75b8f015b483c59e845b9703f07d
tenmaster <tenlion@hotmail.com>
2015-01-07 13:48:55 -0300

    Tournaments -> 3

--- src/net/inaka/pong/rank/Ranker.java
+++ src/net/inaka/pong/rank/Ranker.java
@@ -377,7 +377,7 @@ public class Ranker {
 
 			List<WorksheetEntry> worksheets = inakaPongSheet.getWorksheets();
 			Double age = 0.0;
-			int tournaments = 2;
+			int tournaments = 3;
 			for (WorksheetEntry worksheet : worksheets) {
 				if (tournaments == 0)
 					break;
